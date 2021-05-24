33310c26d89efd3c93dc1d4b8912c7da2914f170
Vitzeno <vitzeno@gmail.com>
2021-05-24 19:05:39 +0100

    Setup basic scene

--- src/canvas.tsx
+++ src/canvas.tsx
@@ -5,42 +5,75 @@ interface Props {
   name: string;
 }
 
-let camera: THREE.PerspectiveCamera,
-  scene: THREE.Scene,
-  renderer: THREE.WebGLRenderer;
-let geometry, material, mesh: THREE.Object3D;
+let camera: THREE.PerspectiveCamera, scene: THREE.Scene;
+let renderer: THREE.WebGLRenderer;
+let geometry;
+let material, cube: THREE.Object3D;
+let shape: THREE.Object3D;
 
-const init = () => {
+const setupCamera = () => {
   camera = new THREE.PerspectiveCamera(
-    70,
+    75,
     window.innerWidth / window.innerHeight,
-    0.01,
-    10
+    0.1,
+    1000
   );
-  camera.position.z = 1;
 
-  scene = new THREE.Scene();
-  geometry = new THREE.BoxGeometry(0.2, 0.2, 0.2);
-  material = new THREE.MeshNormalMaterial();
+  camera.position.z = 10;
+};
 
-  mesh = new THREE.Mesh(geometry, material);
-  scene.add(mesh);
-
+const setupRenderer = () => {
+  let renderer: THREE.WebGLRenderer;
   renderer = new THREE.WebGLRenderer({ antialias: true });
   renderer.setSize(window.innerWidth, window.innerHeight);
   renderer.setAnimationLoop(animation);
 
+  return renderer;
+};
+
+const setupGeometry = () => {
+  geometry = new THREE.BoxGeometry();
+  material = new THREE.MeshBasicMaterial({ color: 0x00ff00, wireframe: true });
+  cube = new THREE.Mesh(geometry, material);
+
+  cube.translateX(5);
+  scene.add(cube);
+};
+
+const setupShape = () => {
+  geometry = new THREE.TetrahedronGeometry(1, 0);
+  material = new THREE.MeshBasicMaterial({
+    color: 0xffff00,
+    wireframe: true,
+  });
+  shape = new THREE.Mesh(geometry, material);
+  scene.add(shape);
+};
+
+const setupScene = () => {
+  scene = new THREE.Scene();
+  setupGeometry();
+  setupShape();
+};
+
+const init = () => {
+  setupCamera();
+  setupScene();
+  renderer = setupRenderer();
   document.body.appendChild(renderer.domElement);
 };
 
 const animation = (time: number) => {
-  mesh.rotation.x = time / 2000;
-  mesh.rotation.y = time / 1000;
+  shape.rotation.x = time / 2000;
+  shape.rotation.y = time / 1000;
+
+  cube.rotation.x = time / 2000;
+  cube.rotation.y = time / 1000;
 
   renderer.render(scene, camera);
 };
 
-export const CreateCanvas: FC<Props> = ({ name }: Props) => {
+export const CreateCanvas: FC<Props> = ({ name }) => {
   useEffect(() => {
     init();
   });
